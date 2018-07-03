ea75963af2c65e1bf9f3da29176944ddd36203f2
Julio <jalfaro@techlaunch.io>
2018-07-02 21:21:15 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// q1
+var varOne = 5*9;
+
+var varTwo 
+// q2
+var h2 = document.querySelector('h2');
+var p = document.querySelector('.class');
+var div = document.querySelector('#id');
+var input = document.querySelector('a')
+var button = document.querySelector('button')
+//q3
+button.addEventListener('click',function(){
+    
+})
